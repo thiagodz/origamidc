15f2bebbb05e172ace3f85296203142f1d96efa0
Thiago Lucas <mldathiago@gmail.com>
2019-09-12 17:48:09 -0300

    javascript slide

---
+++ js/script.js
@@ -0,0 +1,13 @@
+if (window.SimpleSlide) {
+    new SimpleSlide ({
+        slide: "quote",
+        time: 5000
+    });
+    
+    new SimpleSlide ({
+        slide: "portfolio",
+        time: 5000,
+        nav: true
+    });
+}
+
